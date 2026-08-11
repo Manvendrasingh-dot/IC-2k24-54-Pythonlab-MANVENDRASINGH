7f735fe4e5f84ff28761451b2d710e9c90664fc3
Manvendra singh <261639196+Manvendrasingh-dot@users.noreply.github.com>
2026-08-11 21:11:10 +0530

    Add files via upload

---
+++ variable_practice.py.py
@@ -0,0 +1,10 @@
+name = "Manvendra"          # string
+age = 20                    # integer
+height = 5.11               # float (in feet)
+is_student = True            # boolean
+
+
+print("Name:", name, "-> Type:", type(name))
+print("Age:", age, "-> Type:", type(age))
+print("Height:", height, "-> Type:", type(height))
+print("Is Student:", is_student, "-> Type:", type(is_student))
